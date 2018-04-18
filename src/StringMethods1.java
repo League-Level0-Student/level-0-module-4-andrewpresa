c730f3db95ad76aa33c361b661b094ed47081fef
presa.andrew <presa.andrew@SEMS-259-09.SEMS259.LAB>
2018-04-18 16:27:13 -0700

    stringmethods1

--- src/StringMethods1.java
+++ src/StringMethods1.java
@@ -4,25 +4,42 @@
 
 
 public class StringMethods1 {
+	private static final String String = null;
+
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String bigballerbrand="biggiefortheballerz";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(bigballerbrand.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
-
+         System.out.println(bigballerbrand.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
 
+for (int i = 0; i < bigballerbrand.length(); i++) {
+	System.out.println(bigballerbrand.charAt(i));
+	
+}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
+char ThechosenOne = bigballerbrand.charAt(5);
+       
+   for (int i = 0; i < bigballerbrand.length(); i++) {
+	   if(ThechosenOne==bigballerbrand.charAt(i)) {
+		   System.out.println("e is at index 5");
+		   
+	   }
+	
+}
+       
+         
 		
 	}
 }
